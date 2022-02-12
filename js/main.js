5f01df13666981b944fc64352a19d4ffc7baf571
Sanket Jadhav <sanketvj.a01@gmail.com>
2022-02-12 16:21:26 +0530

    Added modal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+// Handle navbar toggle
 const navbarToggler = document.querySelector('#navbar-toggler');
-navbarToggler.addEventListener('click', () => {
-  const navbarMenu = document.querySelector('.navbar-menu');
-  navbarMenu.classList.toggle('navbar-menu-visible');
-});
+if (navbarToggler) {
+  navbarToggler.addEventListener('click', () => {
+    const navbarMenu = document.querySelector('.navbar-menu');
+    navbarMenu.classList.toggle('navbar-menu-visible');
+  });
+}
